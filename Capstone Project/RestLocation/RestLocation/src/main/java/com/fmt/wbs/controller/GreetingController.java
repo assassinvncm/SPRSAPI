28081942160c7bacab53870e0555a324adc85a35
assassinvncm <huntercity00000@gmail.com>
2021-08-16 22:06:44 +0700

    merge websocket

---
+++ Capstone Project/RestLocation/RestLocation/src/main/java/com/fmt/wbs/controller/GreetingController.java
@@ -0,0 +1,22 @@
+package com.fmt.wbs.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.util.HtmlUtils;
+
+import com.fmt.wbs.model.Greeting;
+import com.fmt.wbs.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+
+  @MessageMapping("/hello")
+  @SendTo("/topic/greetings")
+  public Greeting greeting(HelloMessage message) throws Exception {
+    Thread.sleep(1000); // simulated delay
+    return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+  }
+
+}
